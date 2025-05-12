de99e4376bb00004b5e74d83c47af99dcbb23058
Android789515 <derflug@mailfence.com>
2025-05-12 16:57:50 -0500

    feat: install script
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ install.lua
@@ -0,0 +1,9 @@
+local hasGitClone = shell.resolve("gitclone")
+
+if !hasGitClone then
+   shell.run("wget", "https://raw.githubusercontent.com/Konijima/cc-git-clone/master/gitclone.lua")
+end
+
+shell.run("gitclone", "Android789515", "Vbore", "main", "opt")
+
+
