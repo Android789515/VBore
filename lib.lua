2d1d95022bbebc643bd4b199ceb4d6f2a524f2b0
Android789515 <derflug@mailfence.com>
2025-05-15 12:04:29 -0500

    feat: inspect block before moving turtle
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- lib.lua
+++ lib.lua
@@ -1,96 +1,97 @@
 local function traverse(distance, position)
-    if position == distance then
-        Log(string.format("Successfully traversed %i blocks.", distance))
-        return
-    else
-        local movedForward, error = turtle.forward()
+   if position == distance then
+      Log(string.format("Successfully traversed %i blocks.", distance))
+      return
+   else
+      Log(string.format("The block in front of the turtle is: %s", turtle.inspect()))
+      local movedForward, error = turtle.forward()
 
-        if movedForward then
-            Log("Traversing...")
-            traverse(distance, position + 1)
-        else
-            Crash(string.format("Turtle got stuck because of: %s", error))
-        end
-    end
+      if movedForward then
+         Log("Traversing...")
+         traverse(distance, position + 1)
+      else
+         Crash(string.format("Turtle got stuck because of: %s", error))
+      end
+   end
 end
 
 local function home(holeSize)
-    local isEven = holeSize % 2 == 0
+   local isEven = holeSize % 2 == 0
 
-    if isEven then
-        turtle.turnRight()
-        traverse(holeSize, 0)
-        turtle.turnRight()
-    else
-        turtle.turnRight()
-        turtle.turnRight()
-        traverse(holeSize, 0)
+   if isEven then
+      turtle.turnRight()
+      traverse(holeSize, 0)
+      turtle.turnRight()
+   else
+      turtle.turnRight()
+      turtle.turnRight()
+      traverse(holeSize, 0)
 
-        turtle.turnRight()
-        traverse(holeSize, 0)
-        turtle.turnRight()
-    end
+      turtle.turnRight()
+      traverse(holeSize, 0)
+      turtle.turnRight()
+   end
 end
 
 local function digNextSection(rotation)
-    if rotation == 1 then
-        Log("Starting even section...")
-        turtle.turnLeft()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnLeft()
-    else
-        Log("Starting odd section...")
-        turtle.turnRight()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnRight()
-    end
+   if rotation == 1 then
+      Log("Starting even section...")
+      turtle.turnLeft()
+      turtle.dig()
+      turtle.forward()
+      turtle.turnLeft()
+   else
+      Log("Starting odd section...")
+      turtle.turnRight()
+      turtle.dig()
+      turtle.forward()
+      turtle.turnRight()
+   end
 
-    return math.abs(rotation - 1)
+   return math.abs(rotation - 1)
 end
 
 local function excavateToSize(holeSize)
-    local rotation = 0
+   local rotation = 0
 
-    turtle.digDown()
-    turtle.down()
+   turtle.digDown()
+   turtle.down()
 
-    for z = 0, (holeSize - 1), 1 do
-        for x = 0, (holeSize - 2), 1 do
-            Log(string.format("Digging: ( %i, %i )", x, z))
-            turtle.dig()
-            turtle.forward()
-        end
+   for z = 0, (holeSize - 1), 1 do
+      for x = 0, (holeSize - 2), 1 do
+         Log(string.format("Digging: ( %i, %i )", x, z))
+         turtle.dig()
+         turtle.forward()
+      end
 
-        if z < holeSize - 1 then
-            rotation = digNextSection(rotation)
-        end
-    end
+      if z < holeSize - 1 then
+         rotation = digNextSection(rotation)
+      end
+   end
 end
 
 local function hasEnoughFuel(fuelRequirement)
-    return turtle.getFuelLevel() >= fuelRequirement
+   return turtle.getFuelLevel() >= fuelRequirement
 end
 
 local function refuel(slot)
     local maxSlot = 16
 
-    turtle.select(slot)
-    Log(string.format("Selected slot %i for refueling", slot))
+   turtle.select(slot)
+   Log(string.format("Selected slot %i for refueling", slot))
 
-    if turtle.refuel() then
-        Log(string.format("Successfully refueled from slot %i.", slot))
-        return
-    elseif slot <= maxSlot then
-        Log("No fuel in this slot.")
-        refuel(slot + 1)
-    end
+   if turtle.refuel() then
+      Log(string.format("Successfully refueled from slot %i.", slot))
+      return
+   elseif slot <= maxSlot then
+      Log("No fuel in this slot.")
+      refuel(slot + 1)
+   end
 end
 
 local function excavateLayer(holeSize)
-    if hasEnoughFuel(holeSize ^ 2) then
-        Log("Turtle has enough fuel for the next layer.")
+   if hasEnoughFuel(holeSize ^ 2) then
+      Log("Turtle has enough fuel for the next layer.")
 
         if holeSize == 1 then
             turtle.digDown()
@@ -105,20 +106,20 @@ local function excavateLayer(holeSize)
 end
 
 local function digLayerDown(targetDepth, currentDepth, holeSize)
-    if currentDepth == targetDepth then
-        Log(string.format("Target depth of y: %i reached.", targetDepth))
-        return
-    else
-        Log(string.format("Excavating %ix%i layer at y: %i.", holeSize, holeSize, currentDepth))
-        excavateLayer(holeSize)
+   if currentDepth == targetDepth then
+      Log(string.format("Target depth of y: %i reached.", targetDepth))
+      return
+   else
+      Log(string.format("Excavating %ix%i layer at y: %i.", holeSize, holeSize, currentDepth))
+      excavateLayer(holeSize)
 
-        Log("Homing...")
-        home(holeSize)
-    end
+      Log("Homing...")
+      home(holeSize)
+   end
 
-    digLayerDown(targetDepth, currentDepth - 1)
+   digLayerDown(targetDepth, currentDepth - 1)
 end
 
 return {
-    digLayerDown = digLayerDown,
+   digLayerDown = digLayerDown,
 }
